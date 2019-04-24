4b843be331783e90cec3b935be7273cc999d0f73
Isaque Bressy <isaque.ikee@gmail.com>
2019-04-24 09:50:45 -0300

    Add Transactional annotation to PersonRepository

--- src/main/java/br/com/bressy/isaque/contactList/repositories/PersonRepository.java
+++ src/main/java/br/com/bressy/isaque/contactList/repositories/PersonRepository.java
@@ -1,9 +1,11 @@
 package br.com.bressy.isaque.contactList.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.bressy.isaque.contactList.entities.Person;
 
+@Transactional(readOnly = true)
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
 	public Person findByName(String name);
